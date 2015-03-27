0ebc936b797bcede7902a302c2ade4f54057a981
Nishant Upadhyay <nishant.upadhyay@beaverlogic.com>
2015-03-27 11:01:04 +0530

    - Update Header footer - Make Category icons dynamic based on the link category

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,32 @@ module ApplicationHelper
   def is_favourite_link?(link)
     link.favourite
   end
+
+  def set_category_icon(link)
+    category = link.category
+    case category
+      when 'Video'
+        "fa-video-camera"
+      when 'Podcast'
+        "fa-volume-up"
+      when 'BlogPost'
+        "fa-bold"
+      else
+        "fa-file-text"
+      end
+  end
+
+  def set_tool_tip(link)
+    category = link.category
+    case category
+      when 'Video'
+        "Video"
+      when 'Podcast'
+        "Podcast"
+      when 'BlogPost'
+        "Blogpost"
+      else
+        "Tutorial"
+    end
+  end
 end
