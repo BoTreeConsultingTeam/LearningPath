5ae30e9403644acfdd8d3865943d69f2a9b3698c
Hiren.Bhalani-BTC <hiren.bhalani@beaverlogic.com>
2015-03-30 17:05:43 +0530

    changes after merge with lp_phase_1

--- app/controllers/learn_time_controller.rb
+++ app/controllers/learn_time_controller.rb
@@ -2,15 +2,10 @@ class LearnTimeController < ApplicationController
   def create
     @link = Link.find(params[:link_id])
     @link = LearnTime.create!(user_id: current_user.id, link_id: params[:link_id])
-
-    @read_count =
-    # @link.learn_time(current_user)
     redirect_to root_path
   end
 
   def index
     @links = current_user.learn_time
   end
-
-  #  u.learn_time.where("created_at < ?", DateTime.now - 5.minutes)   for specific time
 end
