995dafa385387afbf2a19636edd89241c4ce5d91
Nishant Upadhyay <nishant.upadhyay@beaverlogic.com>
2015-03-30 19:51:16 +0530

    loading image not working

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         $(window).scroll(function() {
             var url = $('.pagination .next_page').attr('href');
             if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-                $('.pagination').html('<img src="/assets/load.gif"  style="margin-bottom: 10px; margin-left: 400px"/>')
+                $('.pagination').html('<img src="load.gif"  style="margin-bottom: 10px; margin-left: 400px"/>')
                 return $.getScript(url);
             }
         });
